45b57d7a0649e0ace6bf9c0bb9ae3f1e8064f55a
phaytsukiming <moonsolo@gmail.com>
2012-01-15 17:06:29 +0800

    add P5

---
+++ haskell/99/P5.hs
@@ -0,0 +1,20 @@
+module P5
+(
+ myReverse,
+ myReverse',
+ myReverse'',
+) where
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+myReverse' = foldl (\acc x->(x:acc)) []
+
+myReverse'' :: [a] -> [a]
+myReverse'' [] = []
+myReverse'' xs = _reverse [] xs
+    where 
+        _reverse ret [] = ret
+        _reverse acc (x:xs) = _reverse (x:acc) xs
+
